285ce7e30b06f253361184207cb04ec1526dd81e
ARuan <3205647203@qq.com>
2023-08-03 17:29:40 +0800

    dev

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from 'vuepress'
 
 export default defineUserConfig({
-    base: '/blog/',
+    base: '/Blog/',
     head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
     // lang: 'zh-CN',
     // title: 'ARuanLY个人博客',
